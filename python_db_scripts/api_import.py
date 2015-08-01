64149dd1289432eaac762dff4b64b996af7d3f0c
Alex Dent <aldent95@hotmail.com>
2015-08-02 11:06:11 +1200

    Fixing git hub issues

--- python_db_scripts/api_import.py
+++ python_db_scripts/api_import.py
@@ -1,7 +1,5 @@
 
 
-import datetime
-import pymongo as pmg
 import json
 import os
 import sys
@@ -15,36 +13,41 @@ rows = []
 with open('data\crash-data-2015.csv','r') as inputFile:
     for line in inputFile:
         if headers == []:
-            headers = line.split(',')
+            headers = line.strip('\n').split(',')
             headers.insert(0,'_id')
         else:
-            rows.append(line)
+            rows.append(line.strip('\n').split(','))
 inputFile.close()
 wellingtonData = []
 print "Loaded"
 print len(rows)
 for row in rows:
     if 'Wellington City' in row:
-        string = '"' + str(rowID) + '",'+row
+        row.insert(0,rowID)
         rowID = rowID +1
-        wellingtonData.append(string)
+        wellingtonData.append(row)
 
-csvdata = csv.DictReader(wellingtonData, headers, delimiter=',')
 
-client= pmg.MongoClient()
-try:
-    db = client['CrashData']
-    collection = db['2015']
-    for row in csvdata:
-        del row[None]
-        collection.insert_one(row)
-    print client.database_names()
 
+
+
+geoObjects = []
+for row in wellingtonData:
+    length = len(row)
+    coords = {'lat': row[length-2], 'lng':row[length-1]}
+    properties = ""
+    for i in xrange(0,length-2):
+        if i != 0:
+            properties += ','
+        temp = '"'+str(headers[i])+'": "' + str(row[i])+'"'
+        properties += temp
+    print coords
+    print properties
+    break
+
+os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__),'..','src','main','resource','public','data')))
+geoFile = open('2015.txt', 'w+')
     
-except Exception as e:
-    client.drop_database('CrashData')
-    print e
-finally:
-    client.close()
-        
-    
+
+
+
