30ad7d7edf3053aeacbabc77627af7b909216292
Alex Dent <aldent95@hotmail.com>
2015-08-02 15:25:10 +1200

    Started adding comments to the parser, reverted to no icon data

--- python_db_scripts/api_import.py
+++ python_db_scripts/api_import.py
@@ -5,31 +5,31 @@ import os
 import sys
 import csv
 
-def logicConversion(properties):
-    icon = {'iconUrl':'temp', 'iconSize': [10,10], 'iconAnchor':[25,25]}
-    if int(properties['CRASH SEV CNT']) > 0:
-        properties['marker-color'] = '#FF3300'
-        properties['marker-size'] = 'large'
-        icon['iconSize']=[50,50]
-        properties['Injuries'] = 'Serious Injuries'
-    elif int(properties['CRASH MIN CNT']) > 0:
-        properties['marker-color'] = '#FFFF00'
-        properties['marker-size'] = 'medium'
-        icon['iconSize']=[25,25]
-        properties['Injuries'] = 'Minor Injuries'
-    elif int(properties['CRASH FATAL CNT']) > 0:
-        properties['marker-color'] = '#000000'
-        properties['marker-size'] = 'large'
-        icon['iconSize']=[50,50]
-        properties['Injuries'] = 'Fatal Injuries'
-    else:
-        properties['marker-color'] = '#3366FF'
-        properties['marker-size'] = 'small'
-        properties['Injuries'] = 'No Injuries'
+def logicConversion(properties): #Method for checking fields in the json data and changing those fields and other fields based on what they contain
+    icon = {'iconUrl':'temp', 'iconSize': [10,10], 'iconAnchor':[25,25]}#Default icon settings
+    if int(properties['CRASH SEV CNT']) > 0: #If crash has severe injuries
+        properties['marker-color'] = '#FF3300' #Change the marker color
+        properties['marker-size'] = 'large' #Change the marker size
+        icon['iconSize']=[50,50] #Change the icon size
+        properties['Injuries'] = 'Serious Injuries' #Add a field for injury text and place text in it
+    elif int(properties['CRASH MIN CNT']) > 0: #If crash has minor injuries
+        properties['marker-color'] = '#FFFF00' #Change the marker color
+        properties['marker-size'] = 'medium' #Change the marker size
+        icon['iconSize']=[25,25] #Change the icon size
+        properties['Injuries'] = 'Minor Injuries' #Add a field for injury text and place text in it
+    elif int(properties['CRASH FATAL CNT']) > 0: #If crash has fatal injuries
+        properties['marker-color'] = '#000000'#Change the marker color
+        properties['marker-size'] = 'large' #Change the marker size
+        icon['iconSize']=[50,50] #Change the icon size
+        properties['Injuries'] = 'Fatal Injuries' #Add a field for injury text and place text in it
+    else: #If there are no injuries
+        properties['marker-color'] = '#3366FF' #Change the marker color
+        properties['marker-size'] = 'small' #Change the marker size
+        properties['Injuries'] = 'No Injuries' #Add a field for injury text and place text in it
 
-    if properties['MVMT'][0] == 'A':
+    if properties['MVMT'][0] == 'A': #Adds correct incident description text based on the code provided
         properties['MVMT'] = 'Overtaking and Lane Change'
-        icon['iconUrl'] = '/images/1.png'
+        icon['iconUrl'] = '/images/1.png' #Also changed the icon depending on the incident code
     elif properties['MVMT'][0] == 'B':
         properties['MVMT'] = 'Head On'
         icon['iconUrl'] = '/images/2.png'
@@ -72,10 +72,10 @@ def logicConversion(properties):
     elif properties['MVMT'][0] == 'Q':
         properties['MVMT'] = 'Miscellaneous'
         icon['iconUrl'] = '/images/12.png'
-    properties['icon'] = icon
+    #properties['icon'] = icon
 
-    light = ''
-    if properties['LIGHT'][0]=='B':
+    light = '' #Stores information for the light string
+    if properties['LIGHT'][0]=='B': #Sets the light string based on the light code given
         light = 'Bright Sun'
     elif properties['LIGHT'][0]=='O':
         light = 'Overcast'
@@ -84,48 +84,52 @@ def logicConversion(properties):
     elif properties['LIGHT'][0]=='D':
         light = 'Dark'
 
-    if properties['LIGHT'][1] == 'O':
+    if properties['LIGHT'][1] == 'O': #Add to the light string based on the second light code if one is given
         properties['LIGHT'] = light + " / Street Lights On"
     elif properties['LIGHT'][1] == 'F':
         properties['LIGHT'] = light + " / Street Lights Off"
     elif properties['LIGHT'][1] == 'N':
         properties['LIGHT'] = light + " / No Street Lights Present"
-    else:
+    else: #If no second code is given, just set the light field with the current light string
         properties['LIGHT'] = light
-    return properties
+    return properties #Return the modified properties
 
 
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-rowID = 0
+os.chdir(os.path.dirname(os.path.abspath(__file__))) #Change the directory to the current source code location
+rowID = 0 #Initilise the unique row ID
 
-year = 2014
-geoObjects = []
-while True:
+year = 2014 #Starting year for the data we are using
+geoObjects = [] #Initilise the geoObjects storage list
+while True: #Just keep looping until we hit 2016
     if year == 2016:
-        break
-    rows = []
-    headers = []
-    with open('data\crash-data-'+str(year)+'.csv','r') as inputFile:        
-        for line in inputFile:
-            if headers == []:
-                headers = line.strip('\n').split(',')
+        break #Once we hit 2016, break as we have no more data
+    #If given more time I would modifiy this to break on IOException rather than a specific year so I don't
+    #need to change code each year
+    rows = [] #Initilise rows storage
+    headers = [] #Initilise headers storage
+    i = 0
+    with open('data\crash-data-'+str(year)+'.csv','r') as inputFile: #Open the current year file as inputFile
+        for line in inputFile: #For each line in the inputFile
+            if i == 0: #If we dont have headers
+                headers = line.strip('\n').split(',') #THen get the headers
                 headers.insert(0,'_id')
-            else:
+                i+=1 #Tells us we have the headers
+            else:#Otherwise strip and split the line and store
                 rows.append(line.strip('\n').split(','))
-    inputFile.close()
-    wellingtonData = []
-    print "Loaded"
-    print len(rows)
-    for row in rows:
-        if 'Wellington City' in row:
-            row.insert(0,rowID)
-            rowID = rowID +1
-            wellingtonData.append(row)
+    inputFile.close() #Be a tidy kiwi and close the file
+    idedData = [] #Stores just data after it has ben given id
+    print "Loaded" #Tells us the file has been loaded
+    print len(rows) #Tells us how many rows were loaded
+    for row in rows: #For each row
+        if 'Wellington City' in row: #Filter to just wellington data
+            row.insert(0,rowID) #Give the row a unique id
+            rowID = rowID +1 #Increment the current ID tracker
+            idedData.append(row) #add the row to idedData
             
 
     
-    for row in wellingtonData:
+    for row in idedData:
         length = len(row)
         coords = [float(row[length-1]),float(row[length-2])]
         properties = {}
