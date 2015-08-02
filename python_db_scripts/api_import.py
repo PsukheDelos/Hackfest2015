a8b10e858736cd31488f57352a9efce75c0def23
Alex Dent <aldent95@hotmail.com>
2015-08-02 14:34:09 +1200

    Modified Parser to deal with logic and marker formatting

--- python_db_scripts/api_import.py
+++ python_db_scripts/api_import.py
@@ -5,11 +5,81 @@ import os
 import sys
 import csv
 
+def logicConversion(properties):
+    if int(properties['CRASH SEV CNT']) > 0:
+        properties['marker-color'] = '#FF3300'
+        properties['marker-size'] = 'large'
+        properties['Injuries'] = 'Serious Injuries'
+    elif int(properties['CRASH MIN CNT']) > 0:
+        properties['marker-color'] = '#FFFF00'
+        properties['marker-size'] = 'medium'
+        properties['Injuries'] = 'Minor Injuries'
+    elif int(properties['CRASH FATAL CNT']) > 0:
+        properties['marker-color'] = '#000000'
+        properties['marker-size'] = 'large'
+        properties['Injuries'] = 'Fatal Injuries'
+    else:
+        properties['marker-color'] = '#3366FF'
+        properties['marker-size'] = 'small'
+        properties['Injuries'] = 'No Injuries'
+
+    if properties['MVMT'][0] == 'A':
+        properties['MVMT'] = 'Overtaking and Lane Change'
+    elif properties['MVMT'][0] == 'B':
+        properties['MVMT'] = 'Head On'
+    elif properties['MVMT'][0] == 'C':
+        properties['MVMT'] = 'Lost Control or Off Road (Straight Roads)'
+    elif properties['MVMT'][0] == 'D':
+        properties['MVMT'] = 'Cornering'
+    elif properties['MVMT'][0] == 'E':
+        properties['MVMT'] = 'Collision with Obstruction'
+    elif properties['MVMT'][0] == 'F':
+        properties['MVMT'] = 'Rear End'
+    elif properties['MVMT'][0] == 'G':
+        properties['MVMT'] = 'Turning Versus Same Direction'
+    elif properties['MVMT'][0] == 'H':
+        properties['MVMT'] = 'Crossing (No Turns)'
+    elif properties['MVMT'][0] == 'J':
+        properties['MVMT'] = 'Crossing (Vehicle Turning)'
+    elif properties['MVMT'][0] == 'K':
+        properties['MVMT'] = 'Merging'
+    elif properties['MVMT'][0] == 'L':
+        properties['MVMT'] = 'Right Against Turn'
+    elif properties['MVMT'][0] == 'M':
+        properties['MVMT'] = 'Manoeuvring'
+    elif properties['MVMT'][0] == 'N':
+        properties['MVMT'] = 'Pedestrians Crossing Road'
+    elif properties['MVMT'][0] == 'P':
+        properties['MVMT'] = 'Pedestrians Other'
+    elif properties['MVMT'][0] == 'Q':
+        properties['MVMT'] = 'Miscellaneous'
+
+    light = ''
+    if properties['LIGHT'][0]=='B':
+        light = 'Bright Sun'
+    elif properties['LIGHT'][0]=='O':
+        light = 'Overcast'
+    elif properties['LIGHT'][0]=='T':
+        light = 'Twilight'
+    elif properties['LIGHT'][0]=='D':
+        light = 'Dark'
+
+    if properties['LIGHT'][1] == 'O':
+        properties['LIGHT'] = light + " / Street Lights On"
+    elif properties['LIGHT'][1] == 'F':
+        properties['LIGHT'] = light + " / Street Lights Off"
+    elif properties['LIGHT'][1] == 'N':
+        properties['LIGHT'] = light + " / No Street Lights Present"
+    else:
+        properties['LIGHT'] = light
+    return properties
+
+
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 rowID = 0
 
-year = 2015
+year = 2012
 geoObjects = []
 while True:
     if year == 2016:
@@ -41,7 +111,7 @@ while True:
         properties = {}
         for i in xrange(0,length-2):
             properties[headers[i]] = row[i]
-        properties['marker-color'] = '#ff69b4'
+        properties = logicConversion(properties)
         geojson_obj = {"type":"Feature","geometry":{"type":"Point","coordinates": coords},'properties':properties}
         geoObjects.append(geojson_obj)
     print len(geoObjects)
@@ -49,7 +119,7 @@ while True:
 
     
 geoFileInput = {"type":"FeatureCollection", "features":geoObjects}
-geoFile = open(os.path.abspath(os.path.join(os.path.dirname(__file__),'..','src','main','resources','public','data', '2015.txt')), 'w+')
+geoFile = open(os.path.abspath(os.path.join(os.path.dirname(__file__),'..','src','main','resources','public','data', 'CrashDataFormated.txt')), 'w+')
 geoFile.write(json.dumps(geoFileInput))
 geoFile.close()
 
