8f2f620d32a38888b4cdf27916b3a8267400016b
Alex Dent <aldent95@hotmail.com>
2015-08-02 15:04:57 +1200

    Added image files, modified parser to add images to the json objects

--- python_db_scripts/api_import.py
+++ python_db_scripts/api_import.py
@@ -6,17 +6,21 @@ import sys
 import csv
 
 def logicConversion(properties):
+    icon = {'iconUrl':'temp', 'iconSize': [10,10], 'iconAnchor':[25,25]}
     if int(properties['CRASH SEV CNT']) > 0:
         properties['marker-color'] = '#FF3300'
         properties['marker-size'] = 'large'
+        icon['iconSize']=[50,50]
         properties['Injuries'] = 'Serious Injuries'
     elif int(properties['CRASH MIN CNT']) > 0:
         properties['marker-color'] = '#FFFF00'
         properties['marker-size'] = 'medium'
+        icon['iconSize']=[25,25]
         properties['Injuries'] = 'Minor Injuries'
     elif int(properties['CRASH FATAL CNT']) > 0:
         properties['marker-color'] = '#000000'
         properties['marker-size'] = 'large'
+        icon['iconSize']=[50,50]
         properties['Injuries'] = 'Fatal Injuries'
     else:
         properties['marker-color'] = '#3366FF'
@@ -25,34 +29,50 @@ def logicConversion(properties):
 
     if properties['MVMT'][0] == 'A':
         properties['MVMT'] = 'Overtaking and Lane Change'
+        icon['iconUrl'] = '/images/1.png'
     elif properties['MVMT'][0] == 'B':
         properties['MVMT'] = 'Head On'
+        icon['iconUrl'] = '/images/2.png'
     elif properties['MVMT'][0] == 'C':
         properties['MVMT'] = 'Lost Control or Off Road (Straight Roads)'
+        icon['iconUrl'] = '/images/3.png'
     elif properties['MVMT'][0] == 'D':
         properties['MVMT'] = 'Cornering'
+        icon['iconUrl'] = '/images/4.png'
     elif properties['MVMT'][0] == 'E':
         properties['MVMT'] = 'Collision with Obstruction'
+        icon['iconUrl'] = '/images/2.png'
     elif properties['MVMT'][0] == 'F':
         properties['MVMT'] = 'Rear End'
+        icon['iconUrl'] = '/images/5.png'
     elif properties['MVMT'][0] == 'G':
         properties['MVMT'] = 'Turning Versus Same Direction'
+        icon['iconUrl'] = '/images/6.png'
     elif properties['MVMT'][0] == 'H':
         properties['MVMT'] = 'Crossing (No Turns)'
+        icon['iconUrl'] = '/images/7.png'
     elif properties['MVMT'][0] == 'J':
         properties['MVMT'] = 'Crossing (Vehicle Turning)'
+        icon['iconUrl'] = '/images/8.png'
     elif properties['MVMT'][0] == 'K':
         properties['MVMT'] = 'Merging'
+        icon['iconUrl'] = '/images/9.png'
     elif properties['MVMT'][0] == 'L':
         properties['MVMT'] = 'Right Against Turn'
+        icon['iconUrl'] = '/images/9.png'
     elif properties['MVMT'][0] == 'M':
         properties['MVMT'] = 'Manoeuvring'
+        icon['iconUrl'] = '/images/10.png'
     elif properties['MVMT'][0] == 'N':
         properties['MVMT'] = 'Pedestrians Crossing Road'
+        icon['iconUrl'] = '/images/11.png'
     elif properties['MVMT'][0] == 'P':
         properties['MVMT'] = 'Pedestrians Other'
+        icon['iconUrl'] = '/images/11.png'
     elif properties['MVMT'][0] == 'Q':
         properties['MVMT'] = 'Miscellaneous'
+        icon['iconUrl'] = '/images/12.png'
+    properties['icon'] = icon
 
     light = ''
     if properties['LIGHT'][0]=='B':
@@ -79,7 +99,7 @@ def logicConversion(properties):
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 rowID = 0
 
-year = 2012
+year = 2014
 geoObjects = []
 while True:
     if year == 2016:
