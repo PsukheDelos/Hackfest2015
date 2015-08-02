e650f7de18cac5ba6d15b27757fbe5aaca55b62e
Alex Dent <aldent95@hotmail.com>
2015-08-02 12:53:27 +1200

    Features added to parser
    Parser is now setup to support multiple years outputted into one file.
    Current configuration now does all wellington data for 2014/15

--- python_db_scripts/api_import.py
+++ python_db_scripts/api_import.py
@@ -8,38 +8,47 @@ import csv
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 rowID = 0
-headers = []
-rows = []
-with open('data\crash-data-2015.csv','r') as inputFile:
-    for line in inputFile:
-        if headers == []:
-            headers = line.strip('\n').split(',')
-            headers.insert(0,'_id')
-        else:
-            rows.append(line.strip('\n').split(','))
-inputFile.close()
-wellingtonData = []
-print "Loaded"
-print len(rows)
-for row in rows:
-    if 'Wellington City' in row:
-        row.insert(0,rowID)
-        rowID = rowID +1
-        wellingtonData.append(row)
-        
 
+year = 2014
 geoObjects = []
-for row in wellingtonData:
-    length = len(row)
-    coords = [float(row[length-1]),float(row[length-2])]
-    properties = {}
-    for i in xrange(0,length-2):
-        properties[headers[i]] = row[i]
-    geojson_obj = {"type":"Feature","geometry":{"type":"Point","coordinates": coords},'properties':properties}
-    geoObjects.append(geojson_obj)
+while True:
+    if year == 2016:
+        break
+    rows = []
+    headers = []
+    with open('data\crash-data-'+str(year)+'.csv','r') as inputFile:        
+        for line in inputFile:
+            if headers == []:
+                headers = line.strip('\n').split(',')
+                headers.insert(0,'_id')
+            else:
+                rows.append(line.strip('\n').split(','))
+    inputFile.close()
+    wellingtonData = []
+    print "Loaded"
+    print len(rows)
+    for row in rows:
+        if 'Wellington City' in row:
+            row.insert(0,rowID)
+            rowID = rowID +1
+            wellingtonData.append(row)
+            
 
+    
+    for row in wellingtonData:
+        length = len(row)
+        coords = [float(row[length-1]),float(row[length-2])]
+        properties = {}
+        for i in xrange(0,length-2):
+            properties[headers[i]] = row[i]
+        geojson_obj = {"type":"Feature","geometry":{"type":"Point","coordinates": coords},'properties':properties}
+        geoObjects.append(geojson_obj)
+    print len(geoObjects)
+    year += 1
+
+    
 geoFileInput = {"type":"FeatureCollection", "features":geoObjects}
-geoFile = open(os.path.abspath(os.path.join(os.path.dirname(__file__),'..','src','main','resources','public','data', '2015.txt')), 'w+')
+geoFile = open(os.path.abspath(os.path.join(os.path.dirname(__file__),'..','src','main','resources','public','data', 'CrashData.geojson')), 'w+')
 geoFile.write(json.dumps(geoFileInput))
 geoFile.close()
 
